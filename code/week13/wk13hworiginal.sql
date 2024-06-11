599006ca0d483dd2bfe346c5064cdff66b56def1
Nicholas Boss <86071846+NicholasBoss@users.noreply.github.com>
2024-06-11 09:09:57 -0600

    first commit

---
+++ code/week13/wk13hworiginal.sql
@@ -0,0 +1,65 @@
+
+USE `university`;
+
+-- ----------------------------------------------------------------------
+-- Query 1: Students, and their birthdays, of students born in September. 
+--          Format the date to look like it is shown in the result set. 
+-- ----------------------------------------------------------------------
+
+
+-- ----------------------------------------------------------------------------
+-- Query 2: Student's age in years and days as of Jan. 5, 2017.  
+--          Sorted from oldest to youngest. No duplicates  
+--          (You can assume a 365 day year and ignore leap day.) 
+--          Hint: Use the mod function to calculate the days left over 
+--                after dividing by full years. 
+--          The 5th column is just the 3rd and 4th column combined with labels.
+-- ----------------------------------------------------------------------------
+
+
+-- ----------------------------------------------------------------------
+-- Query 3: Students taught by John Jensen.
+--          Sort by student's last name.
+-- ----------------------------------------------------------------------
+
+
+-- ----------------------------------------------------------------------
+-- Query 4: Instructors Bryce will have in Winter 2018 
+--          Sort by the faculty's last name
+-- ----------------------------------------------------------------------
+
+
+-- ----------------------------------------------------------------------
+-- Query 5: Students that take Econometrics in Fall 2019
+--          Sort by student's last name.
+-- ----------------------------------------------------------------------
+
+
+-- ----------------------------------------------------------------------
+-- Query 6: A report showing all of Bryce Carlson's courss for Winter
+--          Sort by the name of the course.
+-- ----------------------------------------------------------------------
+
+
+-- ----------------------------------------------------------------------
+-- Query 7: The number of students enrolled for Fall 2019
+-- ----------------------------------------------------------------------
+
+
+-- ----------------------------------------------------------------------
+-- Query 8: The number of courses in each college. 
+--          Sort by college name.
+-- ----------------------------------------------------------------------
+
+
+-- ---------------------------------------------------------------------------
+-- Query 9: The total number of students each professor can teach in Winter.
+--          Sort by that total number of students (teaching capacity).
+-- ---------------------------------------------------------------------------
+
+
+-- ---------------------------------------------------------------------------
+-- Query 10: Each student's total credit load for Fall, 
+--           but only students with a credit load greater than three.
+--           Sort by credit load in descending order. 
+-- ---------------------------------------------------------------------------
